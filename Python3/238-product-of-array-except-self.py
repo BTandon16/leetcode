11a962fa7bfd991bdd3ae9cdd662b05e988431df
BTandon16 <btandon2@wisc.edu>
2025-07-10 18:21:26 -0500

    Added solution to 238, can be improved

---
+++ Python3/238-product-of-array-except-self.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # This method is really slow
+        prefMult = [1] * len(nums)
+        suffMult = [1] * len(nums)
+        answer = [1] * len(nums)
+        for i in range(len(nums)):
+            if (i > 0):
+                prefMult[i] = nums[i - 1] * prefMult[i - 1]
+            # if (i < (len(nums) - 1)):
+                suffMult[(len(nums) - 1) - i] = nums[(len(nums) - 1) - i + 1] * suffMult[(len(nums) - 1) - i + 1]
+            
+        for i in range(len(nums)):   
+            answer[i] = prefMult[i] * suffMult[i]
+        return answer

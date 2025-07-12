e981cba075d18024ef427527a3eee46335c7b249
BTandon16 <btandon2@wisc.edu>
2025-07-11 19:56:32 -0500

    Improved upon the solution of 238

--- Python3/238-product-of-array-except-self.py
+++ Python3/238-product-of-array-except-self.py
@@ -1,15 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # This method is really slow
-        prefMult = [1] * len(nums)
-        suffMult = [1] * len(nums)
-        answer = [1] * len(nums)
-        for i in range(len(nums)):
-            if (i > 0):
-                prefMult[i] = nums[i - 1] * prefMult[i - 1]
-            # if (i < (len(nums) - 1)):
-                suffMult[(len(nums) - 1) - i] = nums[(len(nums) - 1) - i + 1] * suffMult[(len(nums) - 1) - i + 1]
+        # First Solution - This method is really slow
+        
+        # prefMult = [1] * len(nums)
+        # suffMult = [1] * len(nums)
+        # answer = [1] * len(nums)
+        # for i in range(len(nums)):
+        #     if (i > 0):
+        #         prefMult[i] = nums[i - 1] * prefMult[i - 1]
+        #     # if (i < (len(nums) - 1)):
+        #         suffMult[(len(nums) - 1) - i] = nums[(len(nums) - 1) - i + 1] * suffMult[(len(nums) - 1) - i + 1]
             
-        for i in range(len(nums)):   
-            answer[i] = prefMult[i] * suffMult[i]
+        # for i in range(len(nums)):   
+        #     answer[i] = prefMult[i] * suffMult[i]
+        # return answer
+
+        # Second Solution - Improved (probably still not perfect tho)
+        answer = [1] * len(nums)
+        for i in range(1, len(nums)):
+            answer[i] = nums[i - 1] * answer[i - 1]
+
+        suffMult = 1
+        for i in range(len(nums) - 2, -1, -1):
+            suffMult  = suffMult * nums[i + 1]
+            answer[i] = answer[i] * suffMult
+        
         return answer

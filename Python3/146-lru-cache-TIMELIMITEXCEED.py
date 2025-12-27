3d018f258060bed662fec2153840ae790f38560d
BTandon16 <btandon2@wisc.edu>
2025-12-27 14:18:56 -0500

    Updated solutions to 146

---
+++ Python3/146-lru-cache-TIMELIMITEXCEED.py
@@ -0,0 +1,116 @@
+# Time Limit exceeding due to O(n) lookups with a doubly linked list. Try using Dictionary/Hashmaps for an O(1) lookup
+
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+    # Implement the LRUCache class:
+    # LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+    # int get(int key) Return the value of the key if the key exists, otherwise return -1.
+    # void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. 
+    # If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+    # The functions get and put must each run in O(1) average time complexity.
+
+    head = None
+    tail = None
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        currNode = self.head
+        while (currNode is not None):
+            if (currNode.key == key):
+                # UPDATING CACHE
+                if (currNode == self.head or currNode == self.tail):
+                    if (currNode != self.head):
+                        # If currNode is only the tail, no changes needed to cache
+                        return currNode.value
+                    elif (currNode != self.tail):
+                        # If currNode is the head but not tail, then set the next of currNode as the new head
+                        currNode.next.prev = None
+                        self.head = currNode.next
+                    else:
+                        # If currNode is both head and tail, no changes needed to cache
+                        return currNode.value
+                else:
+                    # Remove the currNode from its position in the list
+                    currNode.prev.next = currNode.next
+                    currNode.next.prev = currNode.prev
+                # Put currNode at the tail of the list
+                self.tail.next = currNode
+                currNode.prev = self.tail
+                currNode.next = None
+                self.tail = currNode
+
+                return currNode.value
+            else:
+                currNode = currNode.next
+        return -1
+    
+    def put(self, key: int, value: int) -> None:
+        if (self.capacity == 0):
+            return
+
+        currNode = self.head
+        count = 0
+        while (currNode is not None):
+            count += 1
+            if (currNode.key == key):
+                currNode.value = value
+                # UPDATING CACHE
+                if (currNode == self.head or currNode == self.tail):
+                    if (currNode != self.head):
+                        # If currNode is only the tail, no changes needed to cache
+                        return currNode.value
+                    elif (currNode != self.tail):
+                        # If currNode is the head but not tail, then set the next of currNode as the new head
+                        currNode.next.prev = None
+                        self.head = currNode.next
+                    else:
+                        # If currNode is both head and tail, no changes needed to cache
+                        return currNode.value
+                else:
+                    # Remove the currNode from its position in the list
+                    currNode.prev.next = currNode.next
+                    currNode.next.prev = currNode.prev
+                # Put currNode at the tail of the list
+                self.tail.next = currNode
+                currNode.prev = self.tail
+                currNode.next = None
+                self.tail = currNode
+
+                return
+            else:
+                currNode = currNode.next
+        
+        newNode = Node(key, value)
+        # Remove the head if capacity is reached (head is least recently used)
+        if (count == self.capacity):
+            if (self.capacity != 1):
+                self.head = self.head.next
+            else:
+                self.head = newNode
+                self.tail = newNode
+                return
+        # Add the new node at the tail of the list
+        if (count != 0):
+            self.tail.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+        else:
+            self.head = newNode
+            self.tail = newNode
+        return
+
+# lRUCache = LRUCache(2)
+# lRUCache.put(2, 1)
+# lRUCache.put(1, 1)
+# lRUCache.put(2, 3)
+# lRUCache.put(4, 1)
+# lRUCache.get(1)
+# lRUCache.get(2)
--- Python3/146-lru-cache.py
+++ Python3/146-lru-cache.py
@@ -1,5 +1,6 @@
+# Node class for Nodes in a doubly linked list (for maintaining LRU Cache)
 class Node:
-    def __init__(self, key: int, value: int):
+    def __init__(self, key:int, value: int):
         self.key = key
         self.value = value
         self.next = None
@@ -7,103 +8,67 @@ class Node:
 
 class LRUCache:
     # Implement the LRUCache class:
-    # LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
-    # int get(int key) Return the value of the key if the key exists, otherwise return -1.
-    # void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. 
-    # If the number of keys exceeds the capacity from this operation, evict the least recently used key.
     # The functions get and put must each run in O(1) average time complexity.
 
-    head = None
-    tail = None
-
+    # LRUCache(int capacity) Initialize the LRU cache with positive size capacity
     def __init__(self, capacity: int):
         self.capacity = capacity
+        # Dictionary with a key:Node pair instead of the key:value
+        self.dict = dict()
 
+        # Initialize the doubly linked list
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    # int get(int key) Return the value of the key if the key exists, otherwise return -1.
     def get(self, key: int) -> int:
-        currNode = self.head
-        while (currNode is not None):
-            if (currNode.key == key):
-                # UPDATING CACHE
-                if (currNode == self.head or currNode == self.tail):
-                    if (currNode != self.head):
-                        # If currNode is only the tail, no changes needed to cache
-                        return currNode.value
-                    elif (currNode != self.tail):
-                        # If currNode is the head but not tail, then set the next of currNode as the new head
-                        currNode.next.prev = None
-                        self.head = currNode.next
-                    else:
-                        # If currNode is both head and tail, no changes needed to cache
-                        return currNode.value
-                else:
-                    # Remove the currNode from its position in the list
-                    currNode.prev.next = currNode.next
-                    currNode.next.prev = currNode.prev
-                # Put currNode at the tail of the list
-                self.tail.next = currNode
-                currNode.prev = self.tail
-                currNode.next = None
-                self.tail = currNode
+        currNode = self.dict.get(key)
+        # Check if the key exists
+        if (currNode is None):
+            return -1
+        # Update cache because this key was accessed
+        self.removeNode(currNode)
+        self.addNode(currNode)
+        return currNode.value
 
-                return currNode.value
-            else:
-                currNode = currNode.next
-        return -1
-    
+    # void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. 
+    # If the number of keys exceeds the capacity from this operation, evict the least recently used key.
     def put(self, key: int, value: int) -> None:
-        if (self.capacity == 0):
-            return
-
-        currNode = self.head
-        count = 0
-        while (currNode is not None):
-            count += 1
-            if (currNode.key == key):
-                currNode.value = value
-                # UPDATING CACHE
-                if (currNode == self.head or currNode == self.tail):
-                    if (currNode != self.head):
-                        # If currNode is only the tail, no changes needed to cache
-                        return currNode.value
-                    elif (currNode != self.tail):
-                        # If currNode is the head but not tail, then set the next of currNode as the new head
-                        currNode.next.prev = None
-                        self.head = currNode.next
-                    else:
-                        # If currNode is both head and tail, no changes needed to cache
-                        return currNode.value
-                else:
-                    # Remove the currNode from its position in the list
-                    currNode.prev.next = currNode.next
-                    currNode.next.prev = currNode.prev
-                # Put currNode at the tail of the list
-                self.tail.next = currNode
-                currNode.prev = self.tail
-                currNode.next = None
-                self.tail = currNode
-
-                return
-            else:
-                currNode = currNode.next
-        
         newNode = Node(key, value)
-        # Remove the head if capacity is reached (head is least recently used)
-        if (count == self.capacity):
-            if (self.capacity != 1):
-                self.head = self.head.next
-            else:
-                self.head = newNode
-                self.tail = newNode
-                return
-        # Add the new node at the tail of the list
-        if (count != 0):
-            self.tail.next = newNode
-            newNode.prev = self.tail
-            self.tail = newNode
+        currNode = self.dict.get(key)
+        # Check if the key exists
+        if (currNode is not None):
+            # Update the doubly linked list with the new node as most recently accessed, remove the old node
+            self.removeNode(currNode)
+            self.addNode(newNode)
+            # Update the node in the dictionary
+            self.dict[key] = newNode
         else:
-            self.head = newNode
-            self.tail = newNode
-        return
+            if self.capacity != len(self.dict.keys()):
+                # Add node to the doubly linked list
+                self.addNode(newNode)
+                # Add a new dictionary key-Node pair
+                self.dict[key] = newNode
+            else:
+                self.dict.pop(self.tail.prev.key)
+                self.removeNode(self.tail.prev)
+
+                self.addNode(newNode)
+                self.dict[key] = newNode
+    
+    def addNode(self, node: Node) -> None:
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+    
+    def removeNode(self, node: Node) -> None:
+        prevNode = node.prev
+        nextNode = node.next
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
 
 # lRUCache = LRUCache(2)
 # lRUCache.put(2, 1)
